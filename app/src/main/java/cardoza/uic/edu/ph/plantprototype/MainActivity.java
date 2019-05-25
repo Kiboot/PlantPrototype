3a2c5c69fc29d5326daf2029d2902b866c806946
Kiboot <kiboot@users.noreply.github.com>
2019-05-25 22:59:55 +0800

    preliminary loader test

--- app/src/main/java/cardoza/uic/edu/ph/plantprototype/MainActivity.java
+++ app/src/main/java/cardoza/uic/edu/ph/plantprototype/MainActivity.java
@@ -2,12 +2,20 @@ package cardoza.uic.edu.ph.plantprototype;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.ImageView;
+import android.graphics.drawable.*;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+
+
+        ImageView mIcDownloadAnimator = (ImageView)findViewById(R.id.ic_pro_circle);
+        Drawable drawable = mIcDownloadAnimator.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();}
+}}
