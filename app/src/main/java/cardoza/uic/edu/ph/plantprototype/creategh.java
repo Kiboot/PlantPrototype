853968acd8941f33e44b54dc7bba36c9a7ea312b
Kiboot <kiboot@users.noreply.github.com>
2019-05-28 20:10:46 +0800

    completed create greenhouse activity

--- app/src/main/java/cardoza/uic/edu/ph/plantprototype/creategh.java
+++ app/src/main/java/cardoza/uic/edu/ph/plantprototype/creategh.java
@@ -4,12 +4,20 @@ package cardoza.uic.edu.ph.plantprototype;
 import android.os.Bundle;
 import android.app.Activity;
 
+
+
 public class creategh extends Activity {
+
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Get the view from new_activity.xml
         setContentView(R.layout.creategh);
+
+
     }
+
+
 }
