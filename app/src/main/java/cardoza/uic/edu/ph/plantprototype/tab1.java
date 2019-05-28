5a0f403a82fed3eba0a51e2b864fc4058c1acb2c
Kiboot <kiboot@users.noreply.github.com>
2019-05-28 18:03:45 +0800

    added functionality to add greenhouse button

--- app/src/main/java/cardoza/uic/edu/ph/plantprototype/tab1.java
+++ app/src/main/java/cardoza/uic/edu/ph/plantprototype/tab1.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
-public class tab1 extends Fragment {
+public class tab1 extends Fragment implements OnClickListener{
+
+    FloatingActionButton button;
 
     public static tab1 newInstance() {
         return new tab1();
@@ -23,8 +28,16 @@ public class tab1 extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        button = getView().findViewById(R.id.btn_new);
 
-
+        // Capture button clicks
+        button.setOnClickListener(this);
     }
+    @Override
+    public void onClick(View arg0) {
 
+        // Start NewActivity.class
+        Intent myIntent = new Intent(tab1.this.getActivity(), creategh.class);
+        startActivity(myIntent);
+    }
 }
--- app/src/main/java/cardoza/uic/edu/ph/plantprototype/tab2.java
+++ app/src/main/java/cardoza/uic/edu/ph/plantprototype/tab2.java
@@ -9,22 +9,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
-public class tab2 extends Fragment {
+public class tab2 extends Fragment  {
 
-    public static tab2 newInstance() {
-        return new tab2();
-    }
+
+
+    public static tab2 newInstance() {return new tab2();}
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.tab2, null);
     }
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-
 
     }
 
